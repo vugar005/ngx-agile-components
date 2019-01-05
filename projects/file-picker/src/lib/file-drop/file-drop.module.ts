ebe69476892b0392b636e5de75f50332a28da9ed
vugar005 <abdullayev1.618@hotmail.com>
2019-01-05 04:48:41 -0800

    cloud icon component added to file picker

--- projects/file-picker/src/lib/file-drop/file-drop.module.ts
+++ projects/file-picker/src/lib/file-drop/file-drop.module.ts
@@ -1,3 +1,4 @@
+import { CloudIconComponent } from './cloud-icon/cloud-icon.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import {FileComponent} from './file-drop.component';
@@ -5,6 +6,7 @@ import {FileComponent} from './file-drop.component';
 @NgModule({
   declarations: [
     FileComponent,
+    CloudIconComponent
   ],
   exports: [FileComponent],
   imports: [CommonModule],
